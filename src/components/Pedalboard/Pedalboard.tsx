285654f0ff37ee5ae92f879a3ab8ac853a5ae04c
Jaycee Lydian <scragz@gmail.com>
2025-05-13 14:31:51 -0700

    styling

--- src/components/Pedalboard/Pedalboard.tsx
+++ src/components/Pedalboard/Pedalboard.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import classes from './Pedalboard.module.css';
+import MatrixCanvas from '../MatrixCanvas'; // Import MatrixCanvas
 
 interface PedalboardProps {
   children: React.ReactNode;
 }
 
 const Pedalboard: React.FC<PedalboardProps> = ({ children, ...props }) => {
-  return <div className={classes.pedalboard} {...props}>{children}</div>;
+  return (
+    <div className={classes.pedalboard} {...props}>
+      {children}
+      <MatrixCanvas /> {/* Add MatrixCanvas here */}
+    </div>
+  );
 };
 
 export default Pedalboard;
